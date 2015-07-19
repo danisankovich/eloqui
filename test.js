0a85d9fc69daefbfc0e9d2636ea272b195b9629b
Michael Sankovich <misankovich@gmail.com>
2015-07-19 03:48:37 -0700

    styles, functionality, login-mock-up, multi-lengual abilities, ui/ux

--- test.js
+++ test.js
@@ -1,38 +1,158 @@
 $(document).ready(function() {
-  var pick = window.getSelection() || document.getSelection();
+  var word="";
+  var lang = 'eng'
+
+  $('#language').change(function(){
+    lang = $('input:radio[name=language]:checked').val();
+    console.log(lang);
+  });
+  var wordArray=[];
+  var pick = document.getSelection() || window.getSelection();
   $('p').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if (word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#definition span').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
   $('h1').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if (word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#word').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
   $('h2').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if (word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#word').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
   $('h3').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if (word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#word').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
   $('h4').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if (word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#word').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
   $('h5').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if (word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#word').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
   $('h6').dblclick(function(event) {
     var word = $.trim(pick.toString().toLowerCase());
-    if(word !== "") { console.log(word) }
-    event.stopPropagation();
+    if (word !== "") {
+      $.ajax('https://glosbe.com/gapi/translate?from='+ lang +'&dest=eng&format=json&phrase=' + word, {
+        dataType: 'JSONP'
+      }).success(function(response) {
+        if(wordArray.indexOf(word) > -1) {
+          $('#word').each(function(){
+            if (word === $(this).html())
+            $(this).addClass('highlight');
+          });
+        };
+        if (wordArray.indexOf(word) === -1){
+          wordArray.push(word);
+          var definition = response.tuc[0].meanings[0].text;
+          $('#definition').append("<span id='word'>" + word + "</span>" + " : " + definition + "<br>");
+          console.log(definition);
+        };
+      });
+    }
   });
 });
